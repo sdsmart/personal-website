150d36b004b20766a20b696becfb0daa26cb4bd5
Stephen Smart <stephendsmart@gmail.com>
2019-06-24 17:42:23 -0600

    Set up website structure.

---
+++ app.js
@@ -0,0 +1,24 @@
+// Constants
+const DEFAULT_PORT = 8888
+
+// Initializing express
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+// --- Routing ---
+
+// Index page
+app.get(['/', '/home', '/index'], function(request, response) {
+    response.sendFile(__dirname + '/views/index.html');
+});
+
+// --- End Routing ---
+
+// Setting up static file service
+app.use('/', express.static(__dirname + '/public'));
+
+// Starting server
+http.listen(process.env.PORT || DEFAULT_PORT, () => {
+    console.log(`Server started on port ${DEFAULT_PORT}`);
+});
