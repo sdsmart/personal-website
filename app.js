658e57d91355102b446ec36a0e5481b0984b716a
Stephen Smart <stephendsmart@gmail.com>
2019-06-28 17:46:47 -0600

    Added dynamic content generation with express-handlebars.

--- app.js
+++ app.js
@@ -2,24 +2,42 @@
 const DEFAULT_PORT = 8888
 
 // Initializing express
+var path = require('path');
+var hbs = require('express-handlebars')
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+// Setting up view engine
+app.engine('hbs', hbs({extname: 'hbs',
+                       defaultLayout: 'layout',
+                       layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 // --- Routing ---
 // Index page
 app.get(['/', '/home', '/index'], function(request, response) {
-    response.sendFile(__dirname + '/views/index.html');
+    response.render('index', {title: 'Steve Smart',
+                              index: 'true',
+                              css: ['index.css'],
+                              js: ['index.js']});
 });
 
 // About page
 app.get(['/about', '/me'], function(request, response) {
-    response.sendFile(__dirname + '/views/about.html');
+    response.render('about', {title: 'About Me',
+                              about: 'true',
+                              css: ['about.css'],
+                              js: ['about.js']});
 });
 
 // Resume page
 app.get(['/resume', '/cv'], function(request, response) {
-    response.sendFile(__dirname + '/views/resume.html');
+    response.render('resume', {title: 'Resume',
+                              resume: 'true',
+                              css: ['resume.css'],
+                              js: ['resume.js']});
 });
 // --- End Routing ---
 
